be905eccd47a1294fabc8b0c981fcdcb180c021b
pgf96 <pgfxx1@gmail.com>
2023-02-10 19:58:29 -0500

    add components and data to homepage

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const battlestations = [
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10f72s2/my_loved_ones_new_desk_shelf_to_match_north/",
+        "image": "https://i.redd.it/v8k713z1wsca1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10dym3b/it_took_me_two_years_to_put_this_all_together_so//",
+        "image": "https://i.redd.it/y3vqz9svpjca1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10m84uw/now_i_can_lose_in_4k/",
+        "image": "https://i.redd.it/fpn6j2dd2jea1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10pwmys/my_first_pc/",
+        "image": "https://i.redd.it/a76sk7mmnefa1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/sxan9z/meet_my_actual_setup/",
+        "image": "https://i.redd.it/9so2uih9t1j81.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/vldeih/my_desk_setup_2022/",
+        "image": "https://i.redd.it/7eusj696x0891.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10v8jjw/plants_added_uprages_halted_until_i_can_afford_a/",
+        "image": "https://i.redd.it/0xg2xl0aimga1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10w49zd/i_think_im_done_with_it/",
+        "image": "https://i.redd.it/tck7bojyqtga1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10solah/skylight_setup/",
+        "image": "https://i.redd.it/ogr52pi8l1ga1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10vapjn/my_white_and_pink_for_now_pc_gaming_setup_this/",
+        "image": "https://i.redd.it/cv2sti4vxmga1.jpg",
+    },
+    {
+        "post": "https://www.reddit.com/r/battlestations/comments/10ruzpm/my_corner/",
+        "image": "https://i.redd.it/3vg1rivz4tfa1.jpg",
+    },
+]
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import './HomePage.css'
 import HomePageBanner from "../../components/HomePageBanner/HomePageBanner"
-import Test from '../../components/HomePageBanner/Test';
+import BattleStationList from '../../components/BattleStationList/BattleStationList';
+
+import { battlestations } from '../../data';
+
 const HomePage = () => {
   return (
     <div>
       {/* navbar. after they go past a certain vh render it and sticky it to the top */}
 
       <HomePageBanner />
+      <BattleStationList battlestations={battlestations}/>
         
-      <Test />
+
       <h1>asdf</h1>
     </div>
   )
