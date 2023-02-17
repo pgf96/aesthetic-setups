d9fcfff84714305559fe2ad4d32d7954cc4962a5
pgf96 <pgfxx1@gmail.com>
2023-02-17 11:58:52 -0500

    change mongodb connect

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
-// mongoose.connect(process.env.MONGO_URI)
+// mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.MONGO_URI)
 
 const db = mongoose.connection;
 
