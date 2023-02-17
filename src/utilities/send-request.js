985ade1873b6daa88e6825e326cd8fa61abe2ada
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:28:21 -0500

    refactor services

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -13,8 +13,15 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     // Add token to an Authorization header
     options.headers.Authorization = `Bearer ${token}`;
   }
-  const res = await fetch(url, options);
-  // res.ok will be false if the status code set to 4xx in the controller action
-  if (res.ok) return res.json();
-  throw new Error('Bad Request');
+  try {
+    const res = await fetch(url, options);
+    // res.ok will be false if the status code set to 4xx in the controller action
+    if (res.ok) return res.json();
+    throw new Error('bad request')
+    // const error = new Error('Bad Request')
+  } catch (error) {
+    console.error('error in API request', error)
+    throw error
+
+  }
 }
