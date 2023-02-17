a328b911942c3874aab7d63a7a430e1710ec91fa
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:31:23 -0500

    refactor home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,30 @@
 import './HomePage.css'
+import { useEffect, useState } from 'react';
+import * as battlestationsAPI from '../../utilities/battlestations-api'
 import HomePageBanner from "../../components/HomePageBanner/HomePageBanner"
-import BattleStationList from '../../components/BattleStationList/BattleStationList';
-
-import { battlestations } from '../../data';
+import BattlestationList from '../../components/BattlestationList/BattlestationList';
+import { useNavigate } from 'react-router-dom';
+// import { battlestations } from '../../data';
 
 const HomePage = () => {
+  const [battlestations, setBattlestations] = useState([])
+  
+  useEffect(function() {
+    async function getAllBattlestations() {
+      const allBattlestations = await battlestationsAPI.getAll()
+      setBattlestations(allBattlestations)
+    }
+    getAllBattlestations()
+  },[])
+
+
+
+
   return (
-    <div>
+    <div className='HomePage'>
       {/* navbar. after they go past a certain vh render it and sticky it to the top */}
-
       <HomePageBanner />
-      <BattleStationList battlestations={battlestations}/>
-        
-
-      <h1>asdf</h1>
+      <BattlestationList battlestations={battlestations}/>
     </div>
   )
 }
