2f4854a4aef5065aaa7dd9da0d53bce846b02585
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:31:04 -0500

    add seed data

--- src/data.js
+++ src/data.js
@@ -1,46 +1,46 @@
 export const battlestations = [
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10f72s2/my_loved_ones_new_desk_shelf_to_match_north/",
-        "image": "https://i.redd.it/v8k713z1wsca1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10f72s2/my_loved_ones_new_desk_shelf_to_match_north/",
+        "imageURL": "https://i.redd.it/v8k713z1wsca1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10dym3b/it_took_me_two_years_to_put_this_all_together_so//",
-        "image": "https://i.redd.it/y3vqz9svpjca1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10dym3b/it_took_me_two_years_to_put_this_all_together_so//",
+        "imageURL": "https://i.redd.it/y3vqz9svpjca1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10m84uw/now_i_can_lose_in_4k/",
-        "image": "https://i.redd.it/fpn6j2dd2jea1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10m84uw/now_i_can_lose_in_4k/",
+        "imageURL": "https://i.redd.it/fpn6j2dd2jea1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10pwmys/my_first_pc/",
-        "image": "https://i.redd.it/a76sk7mmnefa1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10pwmys/my_first_pc/",
+        "imageURL": "https://i.redd.it/a76sk7mmnefa1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/sxan9z/meet_my_actual_setup/",
-        "image": "https://i.redd.it/9so2uih9t1j81.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/sxan9z/meet_my_actual_setup/",
+        "imageURL": "https://i.redd.it/9so2uih9t1j81.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/vldeih/my_desk_setup_2022/",
-        "image": "https://i.redd.it/7eusj696x0891.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/vldeih/my_desk_setup_2022/",
+        "imageURL": "https://i.redd.it/7eusj696x0891.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10v8jjw/plants_added_uprages_halted_until_i_can_afford_a/",
-        "image": "https://i.redd.it/0xg2xl0aimga1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10v8jjw/plants_added_uprages_halted_until_i_can_afford_a/",
+        "imageURL": "https://i.redd.it/0xg2xl0aimga1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10w49zd/i_think_im_done_with_it/",
-        "image": "https://i.redd.it/tck7bojyqtga1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10w49zd/i_think_im_done_with_it/",
+        "imageURL": "https://i.redd.it/tck7bojyqtga1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10solah/skylight_setup/",
-        "image": "https://i.redd.it/ogr52pi8l1ga1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10solah/skylight_setup/",
+        "imageURL": "https://i.redd.it/ogr52pi8l1ga1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10vapjn/my_white_and_pink_for_now_pc_gaming_setup_this/",
-        "image": "https://i.redd.it/cv2sti4vxmga1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10vapjn/my_white_and_pink_for_now_pc_gaming_setup_this/",
+        "imageURL": "https://i.redd.it/cv2sti4vxmga1.jpg",
     },
     {
-        "post": "https://www.reddit.com/r/battlestations/comments/10ruzpm/my_corner/",
-        "image": "https://i.redd.it/3vg1rivz4tfa1.jpg",
+        "redditLink": "https://www.reddit.com/r/battlestations/comments/10ruzpm/my_corner/",
+        "imageURL": "https://i.redd.it/3vg1rivz4tfa1.jpg",
     },
 ]
