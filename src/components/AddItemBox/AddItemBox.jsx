6c732d26fe67ef42b2316a7d02aa94d34a5cbfc4
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:28:44 -0500

    add itembox

---
+++ src/components/AddItemBox/AddItemBox.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+
+export default function AddItemBox({handleAddRow}) {
+    const [itemData, setItemData] = useState({
+        name: '',
+        model: ''
+    })
+
+    function handleChange(e) {
+        setItemData((prevData) => ({
+            ...prevData,
+            [e.target.name]: e.target.value,
+        }))
+    }
+    
+    async function handleSubmit(e) {
+        e.preventDefault()
+        handleAddRow(itemData)
+        setItemData(prevData => ({
+            ...prevData,
+            'name':'',
+            'model':''
+        }))
+
+    }
+
+  return (
+    <Box
+      component="form"
+      sx={{color: 'black',
+        '& > :not(style)': { m: 1, width: '25ch', color: 'black'},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" name='name' value={itemData.name} label="name" variant="outlined" onChange={handleChange}/>
+      <TextField id="outlined-basic" name='model' value={itemData.model} label="model" variant="outlined" onChange={handleChange}/>
+      <button onClick={handleSubmit}>submit</button>
+    </Box>
+  );
+}
