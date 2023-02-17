456ce24ab1c4b9aeb82ad5ce7cd64aa490af17c1
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:31:17 -0500

    refactor auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
-import LoginForm from "../../components/LoginForm/LoginForm"
-import SignUpForm from "../../components/SignUpForm/SignUpForm"
-
-
+import './AuthPage.css';
+import LoginForm from "../../components/LoginForm/LoginForm";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
 
 const Authpage = ({setUser}) => {
   const [showLogin, setShowLogin] = useState(true)
+
   return (
     <main className="AuthPage">
-      <div>
-        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
-      </div>
-      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      {showLogin ? 
+      <LoginForm setUser ={setUser} setShowLogin={setShowLogin} showLogin={showLogin}/> 
+      : 
+      <SignUpForm setUser ={setUser} setShowLogin={setShowLogin} showLogin={showLogin}/>}
     </main>
   )
 }
