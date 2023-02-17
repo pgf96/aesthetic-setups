d4b8a5eb5a576263b76c401fbed83ee2b301b74b
pgf96 <pgfxx1@gmail.com>
2023-02-17 10:31:29 -0500

    add route for pending approval

--- routes/api/battlestations.js
+++ routes/api/battlestations.js
@@ -5,19 +5,14 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn')
 const requireAuth = require('../../middleware/requireAuth')
 // const requireAdmin = requireAuth.requireAdmin()
 
-// battlestations ctrl
-
-
-// all approved battlestations
 router.get('/', battlestationsCtrl.index)
 router.get('/pending', ensureLoggedIn , requireAuth.requireAdmin, battlestationsCtrl.indexPending)
 router.get('/:id', battlestationsCtrl.show)
+router.put('/pending/:id', battlestationsCtrl.approvePending)
 router.put('/:id', battlestationsCtrl.update)
 router.put('/:id/items/:itemId', battlestationsCtrl.deleteItem)
 router.delete('/:id', ensureLoggedIn , requireAuth.requireAdmin, battlestationsCtrl.delete)
 router.post('/',  battlestationsCtrl.create)
-
-
 
 
 module.exports = router
