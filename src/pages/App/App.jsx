13e1d12cf02145e309929f73497a8749c2e55d4c
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:31:11 -0500

    refactor main app

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,30 +1,32 @@
 import './App.css';
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate} from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
 import HomePage from '../HomePage/HomePage';
+import NewBattlestationPage from '../NewBattlestationPage/NewBattlestationPage';
+import BattlestationDetailPage from '../BattlestationDetailPage/BattlestationDetailPage'
+import BattlestationPendingListPage from '../BattlestationPendingListPage/BattlestationPendingListPage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
   
   return (
-    <main className="App">
-      { user ?
+    
+    <main className='main'>
+      <NavBar user={user} setUser={setUser} />
         <>
           <Routes>
-            {/* client-side route that renders the component instance if the path matches the url in the address bar */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
+            <Route path="/" element={<HomePage /> } />
+            <Route path="/battlestations/new" element={user ? <NewBattlestationPage /> : <Navigate to="/auth/login" />} />
+            <Route path="/battlestations/:id" element={<BattlestationDetailPage user={user} setUser={setUser}/>} />
+            <Route path="/pending" element={user ? <BattlestationPendingListPage /> : <Navigate to="/auth/login" />} />
+            <Route path="/auth/login" element={!user ? <AuthPage setUser={setUser} /> : <Navigate to="/" />} />
             <Route path="/orders" element={<OrderHistoryPage />} />
-            <Route path="/" element={<HomePage/>} />
           </Routes>
         </>
-        :
-        <AuthPage setUser={setUser}/>
-      }
     </main>
   );
 }
