41d0e31de32a92fb78988ea660e84f45ecfbc993
pgf96 <pgfxx1@gmail.com>
2023-02-17 03:29:45 -0500

    add battlestation table

--- src/components/HomePageBanner/HomePageBanner.jsx
+++ src/components/HomePageBanner/HomePageBanner.jsx
@@ -2,15 +2,15 @@ import './HomePageBanner.css'
 import { useRef, useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 
-
 export default function HomePageBanner() {
     const [isLoaded, setIsLoaded] = useState(false);
     const videoRef = useRef(null);
-    const video = 'https://res.cloudinary.com/db6jsod0y/video/upload/v1676061588/Organic_Lines_4K_Motion_Background_Loop_a7sdke.mp4'
+    const video = 'https://res.cloudinary.com/db6jsod0y/video/upload/v1676621248/pexels-rostislav-uzunov-7670836_2_vodywu.mp4'
+
 
     useEffect(() => {
         if (videoRef.current) {
-            videoRef.current.playbackRate = .75;
+            videoRef.current.playbackRate = .79;
             setTimeout(() => {
                 setIsLoaded(true)
             }, 100)
@@ -19,7 +19,9 @@ export default function HomePageBanner() {
 
     return (
         <div className='banner'>
+            {/* <video ref={videoRef} src={video} autoPlay loop muted */}
             <video ref={videoRef} src={video} autoPlay loop muted
+
                 style={{ opacity: isLoaded ? 1 : 0, 
                 transition: 'opacity 1s ease-in-out' }}/>
             <div className='cover' ></div>
