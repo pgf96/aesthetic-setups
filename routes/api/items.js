bb151603c4c9ca71a91e215e03a890285e5a5fe2
pgf96 <pgfxx1@gmail.com>
2023-02-20 18:08:35 -0500

    add routes and middleware to items routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const itemsCtrl = require('../../controllers/api/items')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
+const requireAuth = require('../../middleware/requireAuth')
 
-router.post('/:id/items', itemsCtrl.create)
-router.delete('/:id/items/:itemId', itemsCtrl.delete)
+router.post('/:id/items', ensureLoggedIn, requireAuth.requireAdmin, itemsCtrl.create)
+router.delete('/:id/items/:itemId', ensureLoggedIn, requireAuth.requireAdmin, itemsCtrl.delete)
 
 module.exports = router
