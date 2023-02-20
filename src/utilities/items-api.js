399fa8142594bc284e143500cc7c92dc807939c5
pgf96 <pgfxx1@gmail.com>
2023-02-20 17:59:22 -0500

    add itemsAPI

---
+++ src/utilities/items-api.js
@@ -0,0 +1,11 @@
+import sendRequest from "./send-request";
+
+const BASE_URL = '/api/items'
+
+export function addItem(itemData, id) {
+    return sendRequest(`${BASE_URL}/${id}/items`, 'POST' , itemData)
+}
+
+export function deleteItem(id, itemId) {
+    return sendRequest(`${BASE_URL}/${id}/items/${itemId}`, 'DELETE')
+}
