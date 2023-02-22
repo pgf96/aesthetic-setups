07da76fe66bd8e987b7100de0d90c69e4f00d670
pgf96 <pgfxx1@gmail.com>
2023-02-22 17:54:28 -0500

    add unauthorized page

---
+++ src/pages/UnauthorizedPage/UnauthorizedPage.jsx
@@ -0,0 +1,11 @@
+import './UnauthorizedPage.css'
+
+export default function UnauthorizedPage() {
+  return (
+    <div className='UnauthorizedPage'>
+        <h1>
+            unauthorized page
+        </h1>
+    </div>
+  )
+}
