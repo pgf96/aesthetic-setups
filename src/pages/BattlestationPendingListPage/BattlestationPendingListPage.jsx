01606ffb57ea97e23ca6aaeb43827dfeb29210c8
pgf96 <pgfxx1@gmail.com>
2023-02-28 16:26:54 -0500

    refactor useState

--- src/pages/BattlestationPendingListPage/BattlestationPendingListPage.jsx
+++ src/pages/BattlestationPendingListPage/BattlestationPendingListPage.jsx
@@ -3,11 +3,12 @@ import * as battlestationsAPI from '../../utilities/battlestations-api'
 import { useState, useEffect } from "react"
 
 export default function BattlestationPendingListPage() {
-    const [pendingList, setPendingList] = useState(null)
+    const [pendingList, setPendingList] = useState([])
 
     useEffect(function() {
         async function getAllPendingBattlestations() {
             const pendingList = await battlestationsAPI.getAllPending()
+            console.log(pendingList)
             setPendingList(pendingList)
         }
         getAllPendingBattlestations()
