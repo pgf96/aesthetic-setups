f9a2302c44f7bb5a6a9a20ad3bc5d970e974c378
pgf96 <pgfxx1@gmail.com>
2023-03-08 17:52:39 -0500

    add update route to item POS

--- routes/api/items.js
+++ routes/api/items.js
@@ -6,5 +6,6 @@ const requireAuth = require('../../middleware/requireAuth')
 
 router.post('/:id/items', ensureLoggedIn, requireAuth.requireAdmin, itemsCtrl.create)
 router.delete('/:id/items/:itemId', ensureLoggedIn, requireAuth.requireAdmin, itemsCtrl.delete)
+router.put('/:id/items', ensureLoggedIn, requireAuth.requireAdmin, itemsCtrl.updateAllPositions)
 
 module.exports = router
