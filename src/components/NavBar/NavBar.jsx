7b385c795ae9445c617052a0bd32323786438a13
pgf96 <pgfxx1@gmail.com>
2023-03-21 15:13:53 -0400

    remove logout border

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,29 +19,32 @@ export default function NavBar({ user, setUser }) {
     return null
   }
   return (
-    // <Container fluid>
-      <Navbar className='navbar' variant='dark' expand="lg" sticky='top' >
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Brand>
-          <img className='logo' src='https://res.cloudinary.com/db6jsod0y/image/upload/v1676591418/The_world_studios-removebg-preview_p1rfcf.png'></img>
-        </Navbar.Brand>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link as={Link} to='/battlestations/new'>Add</Nav.Link>
-            <Nav.Link as={Link} to='/pending'>Pending</Nav.Link>
-          </Nav>
-          <Nav>
-            <UserLogOut user={user} setUser={setUser} />
-            {!user ?
-              <Nav.Link className='login-nav' as={Link} to='/auth/login'>
-                  login
-              </Nav.Link>
-              :
-              ""}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar className='navbar' variant='dark' expand="lg" sticky='top' >
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Brand>
+        <img className='logo' src='https://res.cloudinary.com/db6jsod0y/image/upload/v1676591418/The_world_studios-removebg-preview_p1rfcf.png'></img>
+      </Navbar.Brand>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to='/'>Home</Nav.Link>
+          <Nav.Link as={Link} to='/battlestations/new'>Add</Nav.Link>
+          {/* if the user is an admin */}
+          {
+            user && user.roles.includes('admin') && (
+              <Nav.Link as={Link} to='/pending'>Pending</Nav.Link>
+          )}
+        </Nav>
+        <Nav>
+          <UserLogOut user={user} setUser={setUser} />
+          {!user ?
+            <Nav.Link className='login-nav' as={Link} to='/auth/login'>
+              login
+            </Nav.Link>
+            :
+            ""}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     // </Container>
   );
 }
