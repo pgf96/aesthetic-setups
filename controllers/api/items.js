7bde76d609b476d88471692b918843486adee57a
pgf96 <pgfxx1@gmail.com>
2023-03-22 00:58:56 -0400

    add guest controller functions to items

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -9,6 +9,27 @@ async function create(req,res) {
     res.json(newItemList)
 }
 
+// GUEST
+async function createGuest(req,res) {
+    req.body.user = req.user._id
+    const battlestation = await Battlestation.findById({_id: '63f662e91b6e69d4961170b6'})
+    const newItemList = battlestation.items
+    newItemList.push(req.body)
+    await battlestation.save()
+    res.json(newItemList)
+}
+
+// GUEST
+async function deleteItemGuest (req,res) {
+    const filter = {_id: '63f662e91b6e69d4961170b6'}
+    const update = { $pull: { items: { _id: req.params.itemId}}}
+    const options = {new: true}
+    const updatedBattlestation = await Battlestation.findOneAndUpdate(filter, update, options)
+    res.json(updatedBattlestation)
+
+}
+
+
 async function deleteItem (req,res) {
     const filter = {_id: req.params.id}
     const update = { $pull: { items: { _id: req.params.itemId}}}
@@ -16,6 +37,15 @@ async function deleteItem (req,res) {
     const updatedBattlestation = await Battlestation.findOneAndUpdate(filter, update, options)
     res.json(updatedBattlestation)
 
+}
+
+// GUEST
+async function updateAllPositionsGuest(req, res) {
+    const battlestation = await Battlestation.findOne({_id: '63f662e91b6e69d4961170b6'})
+    battlestation.items = req.body
+    battlestation.save()
+    const updatedItems = battlestation.items
+    res.json(battlestation)
 }
 
 async function updateAllPositions(req, res) {
@@ -28,6 +58,9 @@ async function updateAllPositions(req, res) {
 
 module.exports = {
     create,
+    createGuest,
+    deleteItemGuest,
     delete: deleteItem,
+    updateAllPositionsGuest,
     updateAllPositions
 }
