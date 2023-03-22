a61be5e44718cc2f79f1bca6a8dcc9d5ff11797d
pgf96 <pgfxx1@gmail.com>
2023-03-21 23:57:25 -0400

    refactor tableWidth prop

--- src/pages/BattlestationDetailPage/BattlestationDetailPage.jsx
+++ src/pages/BattlestationDetailPage/BattlestationDetailPage.jsx
@@ -2,7 +2,6 @@
 
 import './BattlestationDetailPage.css'
 import { useParams } from "react-router-dom"
-import { useRef } from 'react'
 import { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom'
 import * as battlestationsAPI from '../../utilities/battlestations-api'
@@ -15,11 +14,18 @@ import { useMediaQuery } from 'react-responsive'
 import { useMemo } from 'react'
 import { scaleLinear } from '@visx/scale'
 
+import { Tooltip } from 'react-tooltip'
+import {BiHelpCircle} from 'react-icons/bi'
+import 'react-tooltip/dist/react-tooltip.css'
 
-export default function BattlestationDetailPage({user, setUser}) {
+
+export default function BattlestationDetailPage({user}) {
+
   const [battlestation, setBattlestation] = useState({})
-  const [clickCoordinates, setClickCoordinates] = useState([]);
   const [loaded, setLoaded] = useState(false)
+  const [isEditable, setIsEditable] = useState(false)
+  const [isSaved, setIsSaved] = useState(false)
+  const [unsavedAnnotation, setUnsavedAnnotation] = useState([])
 
   const { id } = useParams()
   const navigate = useNavigate()
@@ -31,7 +37,6 @@ export default function BattlestationDetailPage({user, setUser}) {
     }
     getById()    
   }, [])
-
 
   async function handleDelete() {
     await battlestationsAPI.deleteOne(id)
@@ -63,11 +68,20 @@ export default function BattlestationDetailPage({user, setUser}) {
   }
 
   async function handleUpdateAllItemPositions(itemPositions) {
-    // console.log(itemPositions)
     const updatedItems = await itemsAPI.updateAllItemPositions(id, itemPositions)
     console.log(updatedItems)
-    setBattlestation(prevData => ({...prevData, items:updatedItems.items}))
-    
+    setBattlestation(prevData => ({...prevData, items:updatedItems.items})) 
+  }
+
+  function handleCheck() {
+    setIsEditable(!isEditable)
+    setIsSaved(false)
+  }
+
+  async function handleAnnotationSave(e) {
+    e.preventDefault()
+    await handleUpdateAllItemPositions(unsavedAnnotation)
+    setIsSaved(true)
   }
 
   const largest = useMediaQuery({query: '(min-width: 1540px)'})
@@ -75,8 +89,10 @@ export default function BattlestationDetailPage({user, setUser}) {
   const medium = useMediaQuery({query: '(min-width: 768px)'})
   const small = useMediaQuery({query: '(min-width: 480px)'})
 
-  const width = largest ? 680 : large ? 850 : medium ? 600 : small ? 500 : 360;
-  const height = largest ? 900 : large ? 643 : medium ? 454 : small ? 378 : 272;
+  const width = largest ? 900 : large ? 850 : medium ? 600 : small ? 500 : 500;
+  const height = largest ? 675 : large ? 637 : medium ? 450 : small ? 375 : 375;
+  
+  const tableWidth = largest ? 450 : large ? 450 : medium ? 450 : small ? 375 : 345;
 
   const xScale = useMemo(
     () =>
@@ -102,24 +118,47 @@ export default function BattlestationDetailPage({user, setUser}) {
   return (
     <div className='BattlestationDetailPage'>
       <Container className='detail'>  
-        <ImageTagger battlestation={battlestation}setLoaded={setLoaded}width={width} height={height} handleUpdateAllItemPositions={handleUpdateAllItemPositions}/>
+        <ImageTagger battlestation={battlestation} setLoaded={setLoaded} unsavedAnnotation={unsavedAnnotation} setUnsavedAnnotation={setUnsavedAnnotation} isEditable={isEditable} width={width} height={height} xScale={xScale} yScale={yScale} handleUpdateAllItemPositions={handleUpdateAllItemPositions}/>
         {loaded && <>
-        <ul>
-          <BattlestationTable user={user} battlestation={battlestation} handleDeleteItem={handleDeleteItem} handleAddItem={handleAddItem}/>
-          <li> Link: <a href={battlestation.redditLink}>Click </a></li>
-          <li> User: {battlestation.redditUser}</li>
+        <div className='display-info'>
+          <span data-tooltip-id='tooltip' data-tooltip-content={`After adding a label, click edit to move the labels. Once you are finished click 'save labels'`}> 
+          <BiHelpCircle  style={{ color: 'white'}}/> 
+          </span>
+          <Tooltip 
+          id='tooltip'
+          />
+          <BattlestationTable tableWidth={tableWidth}user={user} battlestation={battlestation} handleDeleteItem={handleDeleteItem} handleAddItem={handleAddItem}/>
+          <ul className='user-info'>
+          <li style={{ float: 'left'}}> User: {battlestation.redditUser}</li>
+          <li style={{  float: 'right'}} ><a href={battlestation.redditLink}> Link </a></li>
+          </ul>
+
           {battlestation.approved ?
              "": <li style={{color: 'red'}}> "not approved"</li>}
-          {/* display approval button if not */}
+          {/* display approval button if not - refactor to check serverside roles instead of client side*/}
           {user && user.roles.includes('admin') && 
           <>
-            <button onClick={handleDelete}> Delete </button>  
+          <div>
+                    <label style={{ color: 'white'}}> Edit </label>
+                    &nbsp;
+                    <input type="checkbox" name='edit' onChange={handleCheck} />
+                    <br />
+                    {isEditable && (
+                      
+                      <button className='button-control' onClick={handleAnnotationSave}> Save labels </button>
+                    )}
+                    {isSaved && isEditable && (
+                      <span style={{color: 'green'}}> Labels saved! </span>
+                      )}
+            </div>
+            <button className='button-control' onClick={handleDelete}> Delete </button>  
             {battlestation.approved === !true &&
-            <button onClick={handleApprove}> approve </button> 
+            <button className='button-control' onClick={handleApprove}> approve </button> 
             }
+             
           </>
           }
-        </ul>
+        </div>
         </>}
       </Container>
       {loaded && <Footer /> }
