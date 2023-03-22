01a5da7a74125b10dffe8d7fc988c090001050af
pgf96 <pgfxx1@gmail.com>
2023-03-21 23:57:31 -0400

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,20 @@
 import { Container } from 'react-bootstrap'
 import './Footer.css'
+import {FaLinkedinIn, FaGithub} from "react-icons/fa";
 
 export default function Footer() {
     return (
         <Container fluid className='footer-container'>
-            <footer class="footer-bs">
-                <div class="row">
-                    <div class="col-md-5 footer-brand animated fadeInLeft">
-                        <img className='logo' src='https://res.cloudinary.com/db6jsod0y/image/upload/v1676591418/The_world_studios-removebg-preview_p1rfcf.png'></img>
-                        <p>Suspendisse hendrerit tellus laoreet luctus pharetra. Aliquam porttitor vitae orci nec ultricies. Curabitur vehicula, libero eget faucibus faucibus, purus erat eleifend enim, porta pellentesque ex mi ut sem.</p>
-                        <p>© 2014 BS3 UI Kit, All rights reserved</p>
+            <footer className="footer-bs">
+                <div className="row">
+                    <div class="col-md-2 footer-brand animated fadeInLeft">
+                        <img className='logo-footer' src='https://res.cloudinary.com/db6jsod0y/image/upload/v1676591418/The_world_studios-removebg-preview_p1rfcf.png'></img>
                     </div>
-                    <div class="col-md-5 footer-nav animated fadeInUp">
-                        <h4>Menu —</h4>
-                        <div class="col-md-6">
-                            {/* <ul class="pages">
-                                <li><a href="#">Travel</a></li>
-                                <li><a href="#">Nature</a></li>
-                                <li><a href="#">Explores</a></li>
-                                <li><a href="#">Science</a></li>
-                                <li><a href="#">Advice</a></li>
-                            </ul> */}
-                        </div>
-                        <div class="col-md-5">
-                            <ul class="list">
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Contacts</a></li>
-                                <li><a href="#">Terms & Condition</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-2 footer-social animated fadeInDown">
-                        <h4>Follow Us</h4>
+                    <div class="col-md-5 footer-social animated fadeInDown">
+                        <h4>Contact Me</h4>
                         <ul>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Twitter</a></li>
-                            <li><a href="#">Instagram</a></li>
-                            <li><a href="#">RSS</a></li>
+                            <li><a href="https://www.linkedin.com/in/patrickfeliciano/"><FaLinkedinIn /> </a></li>
+                            <li><a href="https://github.com/pgf96"><FaGithub /></a></li>
                         </ul>
                     </div>
                     {/* <div class="col-md-3 footer-ns animated fadeInRight">
