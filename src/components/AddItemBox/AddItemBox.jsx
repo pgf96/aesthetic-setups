69376b1d2da80c9df6a4eb9edb2982a1a974bf50
pgf96 <pgfxx1@gmail.com>
2023-03-21 23:55:18 -0400

    add media query for mobile viewport on buton

--- src/components/AddItemBox/AddItemBox.jsx
+++ src/components/AddItemBox/AddItemBox.jsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
+import {AiOutlineCheckCircle} from 'react-icons/ai'
+import { Button } from '@mui/material';
+import './AddItemBox.css'
+import { useMediaQuery } from 'react-responsive'
 
-export default function AddItemBox({handleAddItem}) {
+export default function AddItemBox({handleAddItem, tableWidth}) {
     const [itemData, setItemData] = useState({
         name: '',
         model: ''
     })
+
+    const smallViewport = useMediaQuery({query: '(max-width: 480px)'})
 
     function handleChange(e) {
         setItemData((prevData) => ({
@@ -31,14 +37,15 @@ export default function AddItemBox({handleAddItem}) {
     <Box
       component="form"
       sx={{color: 'black',
-        '& > :not(style)': { m: 1, width: '25ch', color: 'black'},
+        '& > :not(style)': { m: .75, width: smallViewport ? '140px': '200px', color: 'black'},
       }}
       noValidate
       autoComplete="off"
     >
       <TextField id="outlined-basic" name='name' value={itemData.name} label="name" variant="outlined" onChange={handleChange}/>
       <TextField id="outlined-basic" name='model' value={itemData.model} label="model" variant="outlined" onChange={handleChange}/>
-      <button onClick={handleSubmit}>submit</button>
+      {/* <br /> */}
+      <button className='submit-button' onClick={handleSubmit}> <AiOutlineCheckCircle /> </button>
     </Box>
   );
 }
