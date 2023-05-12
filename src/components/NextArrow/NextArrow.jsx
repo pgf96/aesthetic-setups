7ab4eb490c77912509f9d962c027ea79e35debf2
pgf96 <pgfxx1@gmail.com>
2023-05-11 22:02:28 -0400

    add arrow to landing page

---
+++ src/components/NextArrow/NextArrow.jsx
@@ -0,0 +1,11 @@
+import './NextArrow.css'
+export default function NextArrow() {
+  return (
+    <div className="arrow">
+    <span></span>
+    <span></span>
+    <span></span>
+    </div>
+
+  )
+}
