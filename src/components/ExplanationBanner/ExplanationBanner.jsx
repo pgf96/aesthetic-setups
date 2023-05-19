073366a23987e8c9fe6e2b2e47b9ee8cefadf877
pgf96 <pgfxx1@gmail.com>
2023-05-19 11:34:32 -0400

    Add banner

--- src/components/ExplanationBanner/ExplanationBanner.jsx
+++ src/components/ExplanationBanner/ExplanationBanner.jsx
@@ -4,39 +4,11 @@ import { BsGraphUp } from 'react-icons/bs'
 
 export default function ExplanationBanner() {
     return (
-        <Container maxWidth='xl'>
-            <div className='explanation-banner'>
-                {/* productivity */}
-                <div className='productivity-icon'>
-                    <BsGraphUp />
-                </div>
-                <div className='productivity-desc'>
-                    <p>productivity</p>
-                    <p>asdf</p>
-                </div>
-                {/* organization */}
-                <div className='organization-icon'>
-                    <BsGraphUp />
-                </div>
-                <div className='organization-desc'>
-                    <p>organization</p>
-                </div>
-                {/* creativity */}
-                <div className='creativity-icon'>
-                    <BsGraphUp />
-                </div>
-                <div className='creativity-desc'>
-                    <p>creativity</p>
-                </div>
-                {/* comfort */}
-                <div className='comfort-icon'>
-                    <BsGraphUp />
-                </div>
-                <div className='comfort-desc'>
-                    <p>comfort</p>
-                </div>
 
-            </div>
+        <Container maxWidth='xl'>
+           <div>
+            <h1>how it works</h1>
+           </div>
         </Container>
     )
 }
